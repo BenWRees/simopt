99b1aff4f53e4b1e2f650399dacae83f99692cad
BenWRees <B.Rees@soton.ac.uk>
2025-03-04 22:11:19 +0000

    Experiment to test multiple solvers on a test suite of problems

--- demo/demo_problems_solvers.py
+++ demo/demo_problems_solvers.py
@@ -12,8 +12,38 @@ sys.path.append(
 )  # type:ignore
 
 # Import the ProblemsSolvers class and other useful functions
-from simopt.experiment_base import ProblemsSolvers, plot_solvability_profiles
+from simopt.experiment_base import ProblemsSolvers, plot_solvability_profiles, ProblemSolver
+from simopt.directory import problem_directory
 
+
+def build_problem_solvers(solver_names: list[str], problem_names: list[str]) -> list[list[ProblemSolver]] :
+    """Write out all the ProblemSolver pairs for the experiments 
+
+    Args:
+        solver_names (list[str]): names of the different solvers
+        problem_names (list[str]): names of the different problems to test
+
+    Returns:
+        list[list[ProblemSolver]]: A list of lists of Problem Solvers. Each list in the list of lists share a common solver 
+    """
+    problem_instances = [problem_directory[a]() for a in problem_names]
+
+    problem_solver_pairs = [] 
+
+    for solver in solver_names :
+        common_solvers = [] 
+        solver_factors = None
+        if solver == 'TRUSTREGION' : 
+            solver_factors = {'geometry instance': 'AstroDFGeometry', 'sampling rule': 'AdaptiveSampling', 'model type': 'RandomModelReuse', 'polynomial basis': 'AstroDFBasis'}
+       
+        for problem in problem_instances :
+            problem_solver = ProblemSolver(solver_name=solver, problem=problem, solver_fixed_factors=solver_factors)
+            common_solvers.append(problem_solver)
+        
+        problem_solver_pairs.append(common_solvers)
+
+    return problem_solver_pairs
+        
 
 def main() -> None:
     # !! When testing a new solver/problem, first go to directory.py.
@@ -24,19 +54,25 @@ def main() -> None:
     # Specify the names of the solver and problem to test.
     # These names are strings and should match those input to directory.py.
     # Ex:
-    solver_names = ["TRUSTREGION", "OMoRF", "ASTRODF"]
-    problem_names = ["DYNAMNEWS-1"]
+    solver_names = ["TRUSTREGION", "ASTRODF"]
+    problem_names = ["SIMPLEFUNC-1", "EXAMPLE-1", "DYNAMNEWS-1", "NETWORK-1"] #? TRUSTREGION seems to not like NETWORK-1
+
+
+    problem_solver_pairs = build_problem_solvers(solver_names, problem_names)
+
+    mymetaexperiment = ProblemsSolvers(experiments=problem_solver_pairs)
 
     # Initialize an instance of the experiment class.
-    mymetaexperiment = ProblemsSolvers(
-        solver_names=solver_names, problem_names=problem_names, solver_factors=[{'geometry instance': 'AstroDFGeometry', 'sampling rule': 'AdaptiveSampling', 'model type': 'RandomModelReuse'},{}, {}]
-    )
+    mymetaexperiment = ProblemsSolvers(experiments=problem_solver_pairs)
+    # mymetaexperiment = ProblemsSolvers(solver_names=solver_names, problem_names=problem_names, solver_factors=[{'geometry instance': 'AstroDFGeometry', 'sampling rule': 'AdaptiveSampling', 'model type': 'RandomModelReuse'},{}])
 
     # Write to log file.
     mymetaexperiment.log_group_experiment_results()
 
+    [myexperiment.log_experiment_results() for myexperiments in mymetaexperiment.experiments for myexperiment in myexperiments]
+
     # Run a fixed number of macroreplications of each solver on each problem.
-    mymetaexperiment.run(n_macroreps=3)
+    mymetaexperiment.run(n_macroreps=5)
 
     print("Post-processing results.")
     # Run a fixed number of postreplications at all recommended solutions.
