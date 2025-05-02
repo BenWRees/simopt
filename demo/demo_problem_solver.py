9618ae9a9991f5a6e75a5d720da6057d859c3876
BenWRees <B.Rees@soton.ac.uk>
2025-05-02 10:38:36 +0100

    Rewritten to just produce pickle files for problem-solver pairs

--- demo/demo_problem_solver.py
+++ demo/demo_problem_solver.py
@@ -48,60 +48,52 @@ def main(solver_name, problem_name, solver_factors, problem_factors) -> None :
 	# solver_factors = {'geometry instance': 'AstroDFGeometry', 'sampling rule': 'AdaptiveSampling', 'model type': 'RandomModelReuse', 'polynomial basis': 'AstroDFBasis'}
 	# solver_factors = {'subspace dimension': 1}
 	# Initialize an instance of the experiment class.
-	myexperiment = ProblemSolver(solver_name, problem_name, solver_fixed_factors=solver_factors, problem_fixed_factors=problem_factors)
+	myexperiment = ProblemSolver(solver_name, problem_name, solver_fixed_factors=solver_factors, problem_fixed_factors=problem_factors)#, model_fixed_factors={'mu': 18.0})
 	# myexperiment = ProblemSolver(solver_name, problem_name)
 	# Run a fixed number of macroreplications of the solver on the problem.
-	myexperiment.run(n_macroreps=1)
+	myexperiment.run(n_macroreps=20)
+
+	print(f"Finished running solver {solver_name} on problem {problem_name}.")
 
 	# If the solver runs have already been performed, uncomment the
 	# following pair of lines (and uncommmen the myexperiment.run(...)
 	# line above) to read in results from a .pickle file.
 	# myexperiment = read_experiment_results(file_name_path)
 
-	print("Post-processing results.")
-	# Run a fixed number of postreplications at all recommended solutions.
-	myexperiment.post_replicate(n_postreps=20)
-	# Find an optimal solution x* for normalization.
-	post_normalize([myexperiment], n_postreps_init_opt=20)
+	# print("Post-processing results.")
+	# # Run a fixed number of postreplications at all recommended solutions.
+	# myexperiment.post_replicate(n_postreps=200)
+	# # Find an optimal solution x* for normalization.
+	# post_normalize([myexperiment], n_postreps_init_opt=200)
 
-	# Log results.
-	myexperiment.log_experiment_results()
-	myexperiment.log_experiments_csv()
+	# # Log results.
+	# myexperiment.log_experiment_results()
+	# myexperiment.log_experiments_csv()
 
-	print("Plotting results.")
-	# Produce basic plots of the solver on the problem.
-	plot_progress_curves(
-		experiments=[myexperiment], plot_type="all", normalize=False, plot_title=f'All {solver_name} on {problem_name} using subspace {myexperiment.solver.factors['subspace dimension']}',
-	)
-	plot_progress_curves(
-		experiments=[myexperiment], plot_type="mean", normalize=False, plot_title=f'Mean {solver_name} on {problem_name} using subspace {myexperiment.solver.factors['subspace dimension']}',
-	)
-	plot_progress_curves(
-		experiments=[myexperiment],
-		plot_type="quantile",
-		beta=0.90,
-		normalize=False,
-		plot_title=f'Quantile {solver_name} on {problem_name} using subspace {myexperiment.solver.factors['subspace dimension']}',
-	)
-	plot_solvability_cdfs(experiments=[myexperiment], solve_tol=0.1, plot_title=f'Solvability {solver_name} on {problem_name} using subspace {myexperiment.solver.factors['subspace dimension']}')
-
-
-	# Plots will be saved in the folder experiments/plots.
-	print("Finished. Plots can be found in experiments/plots folder.")
+	# print("Plotting results.")
+	# # # sub_dim = myexperiment.solver.factors['subspace dimension']
+	# # sub_dim = 15
+	# # # Produce basic plots of the solver on the problem.
+	# plot_progress_curves(
+	# 	experiments=[myexperiment], plot_type="all", normalize=False, plot_title=f'All {solver_name} on {problem_name}',
+	# )
+	# plot_progress_curves(
+	# 	experiments=[myexperiment], plot_type="mean", normalize=False, plot_title=f'Mean {solver_name} on {problem_name}',
+	# )
+	# plot_progress_curves(
+	# 	experiments=[myexperiment],
+	# 	plot_type="quantile",
+	# 	beta=0.90,
+	# 	normalize=False,
+	# 	plot_title=f'Quantile {solver_name} on {problem_name}',
+	# )
+	# plot_solvability_cdfs(experiments=[myexperiment], solve_tol=0.1, plot_title=f'Solvability {solver_name} on {problem_name}')
 
 
-def read_line_of_file(line: str) -> tuple[str, str, dict, dict] :
-	solver_name, problem_name, solver_fixed_factors_str, problem_fixed_factors_str = line.split('\\')
-
-	solver_fixed_factors = json.loads(solver_fixed_factors_str)
-	problem_fixed_factors = json.loads(problem_fixed_factors_str)
-
-	return problem_name, solver_name, solver_fixed_factors, problem_fixed_factors
+	# # Plots will be saved in the folder experiments/plots.
+	# print("Finished. Plots can be found in experiments/plots folder.")
 
 if __name__ == "__main__":
-	main('ASTROMoRF', 'DYNAMNEWS-1', {'subspace dimension': 7}, {'budget': 400})
-	# file_path = '/Users/benjaminrees/Desktop/params.txt'
-	# with open(file_path, 'r') as f :
-	# 	for line in f : 
-	# 		problem_name, solver_name, solver_fixed_factors, problem_fixed_factors = read_line_of_file(line)
-	# 		main(solver_name, problem_name, solver_fixed_factors, problem_fixed_factors)
+	main('ASTROMoRF', 'FACSIZE-1',{'crn_across_solns': False}, {'budget': 1000})
+	main('ASTRODF', 'FACSIZE-1',{'crn_across_solns': False}, {'budget': 1000})
+	main('OMoRF', 'FACSIZE-1',{'crn_across_solns': False}, {'budget': 1000})
